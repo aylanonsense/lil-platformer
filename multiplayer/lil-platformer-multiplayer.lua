91caae2f589261273028e4c80e4264978be4e0c5
bridgs <bridgs.dev@gmail.com>
2019-03-09 23:54:00 -0500

    Fixing cover image and color

--- multiplayer/lil-platformer-multiplayer.lua
+++ multiplayer/lil-platformer-multiplayer.lua
@@ -2,6 +2,6 @@
 main: main.lua
 name: lil platformer (multiplayer)
 username: bridgs
-primaryColor: FBEEE6
+primaryColor: 0FD9F6
 coverImage: cover-image.png
 description: A lil platformer with a buncha bouncy lil cloud gals.
